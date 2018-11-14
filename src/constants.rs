9d80205566763e28aa9540f1701bd5b900cdf06e
doohochang <doohochang@gmail.com>
2018-11-14 16:59:33 +0900

    Beautify code style

--- src/constants.rs
+++ src/constants.rs
@@ -1,1 +1,2 @@
 pub const N_DIMENSION: usize = 39;
+pub const WORD_PENALTY: f64 = 70f64;
--- src/fileutil.rs
+++ src/fileutil.rs
@@ -1,11 +1,16 @@
 use std::path::Path;
-use std::fs::File;
-use std::io::Read;
+use std::fs::{self, File};
+use std::io::{self, Read};
+use std::ffi::OsString;
 
 pub fn read_lines(file_path: &str) -> Vec<String> {
     let path = Path::new(file_path);
     let file = File::open(path).expect("Can't open file");
     read_lines_from_file(file)
+}
+
+pub fn list_test_files(directory_path: &str) -> Vec<OsString> {
+    list_files(Path::new(directory_path)).expect("Can't list test files")
 }
 
 fn read_lines_from_file(mut file: File) -> Vec<String> {
@@ -15,4 +20,25 @@ fn read_lines_from_file(mut file: File) -> Vec<String> {
     buffer.lines()
         .map(|s: &str| String::from(s))
         .collect()
+}
+
+fn list_files(path: &Path) -> io::Result<Vec<OsString>> {
+    let mut files = Vec::new();
+
+    if path.is_dir() {
+        for entry in fs::read_dir(path)? {
+            let child_path = entry?.path();
+            if child_path.is_dir() {
+                let child_files = list_files(&child_path)?;
+                files.extend(child_files);
+            } else {
+                files.push(child_path.as_os_str().to_os_string());
+            }
+        }
+
+        Ok(files)
+    } else {
+        files.push(path.as_os_str().to_os_string());
+        Ok(files)
+    }
 }
