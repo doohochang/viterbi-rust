a4a4bca7263f1cf74455f66971ee9eaf50fee5e3
doohochang <doohochang@gmail.com>
2018-12-02 14:54:05 +0900

    Remove Pdf and State structure

--- src/phone/mod.rs
+++ src/phone/mod.rs
@@ -1,35 +1,16 @@
-use std::fmt;
-use constants::*;
-
 pub mod read;
+
 pub use self::read::read_phones as read_all;
 
 #[derive(Debug)]
 pub struct Phone {
     pub index: usize,
     pub name: String,
-    pub states: Vec<State>,
+    pub n_states: usize,
     pub in_prob: Vec<f64>,
     pub trans_prob: Vec<Vec<f64>>,
     pub out_prob: Vec<f64>,
     pub skip_prob: f64, // transition prob between entry and exit
-}
-
-#[derive(Debug)]
-pub struct State {
-    pub pdfs: Vec<Pdf>,
-}
-
-pub struct Pdf {
-    pub weight: f64,
-    pub mean: [f64; N_DIMENSION],
-    pub var: [f64; N_DIMENSION],
-}
-
-impl fmt::Debug for Pdf {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Pdf {{ weight: {}, mean: {:?}.., var: {:?}.. }}", self.weight, &self.mean[0..6], &self.var[0..6])
-    }
 }
 
 pub fn find<'a>(name: &str, phones: &'a [Phone]) -> &'a Phone {
